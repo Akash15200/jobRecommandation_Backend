11f65e43c9dfb9f16ad862e454d53e884e3d200c
Anurag224132 <1pandeyanurag1@gmail.com>
2025-07-15 10:15:38 +0530

    fix url on backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
     origin:"*",
     // credentials: true
 }));
